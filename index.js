17a3044ad987065d0e36175af95efcbfe6c74306
Fabiandgo <fdgo.9910@gmail.com>
2025-11-21 15:01:43 +0100

    aaa  index.js

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ const mongoose = require("mongoose");
 const app = require("./app");
 const config = require("./config")
 
-mongoose.connect("mongodb+srv://fdgo9910_db_user:Fabi.123@fabian.pmm8y8v.mongodb.net/?appName=Fabian", (err, res) => {
-  if (err) throw err;
-  console.log("consexion exitosa");
+mongoose.connect("mongodb+srv://fdgo9910_db_user:Fabi.123@fabian.pmm8y8v.mongodb.net/?appName=Fabian", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  ssl: true, // Asegúrate de que SSL esté habilitado
+})
+  .then(() => {
+    console.log('Conectado a la base de datos MongoDB');
+  })
+  .catch(err => {
+    console.error('Error al conectar a la base de datos:', err);
 
-  app.listen(config.port, () => {
-    console.log("API REST corriendoooooooo");
+
+    app.listen(config.port, () => {
+      console.log("API REST corriendoooooooo");
+    });
   });
-});
