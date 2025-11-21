5e88dce40d4c223a5b2fdc23f51aee566fd5f234
Fabiandgo <fdgo.9910@gmail.com>
2025-11-21 14:45:47 +0100

    aa

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
     module.exports = {
         port: process.env.PORT || 5000,
-        db: process.env.MONGODB_URI || "mongodb+srv://fdgo9910_db_user:Fabi.123@fabian.pmm8y8v.mongodb.net/?appName=Fabian",
+        db:"mongodb+srv://fdgo9910_db_user:Fabi.123@fabian.pmm8y8v.mongodb.net/?appName=Fabian",
         JWT_SECRET: process.env.JWT_SECRET || 'tu_super_secreto_jwt',
         JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || '1d',
         NODE_ENV: process.env.NODE_ENV || 'development'
